6523920d2eabc11306f962610d9176a89eb86cc6
Qol-By <Qolbyadha04@gmail.com>
2023-04-13 14:22:29 +0700

    feat: add endpoint get list, object, and save pets

--- Feature/Pets/Models/PetModels.cs
+++ Feature/Pets/Models/PetModels.cs
@@ -1,8 +1,12 @@
-﻿namespace VetApp_BE.Feature.Pets.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VetApp_BE.Feature.Pets.Models
 {
+
+    [Table("Pets")]
     public class PetModels
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Name { get; set; }
         public string OwnerName { get; set; }
         public string PreferredContactDetails { get; set; }
--- Feature/Pets/ViewModels/PetViewModels.cs
+++ Feature/Pets/ViewModels/PetViewModels.cs
@@ -2,6 +2,7 @@
 {
     public class PetViewModels
     {
+        public int? Id { get; set; }
         public string Name { get; set; }
         public string OwnerName { get; set; }
         public string PreferredContactDetails { get; set; }
